d7edb1ba1c93255a2713a45f7faa4270301b8ab3
C0C24015 <c0c240159c@edu.teu.ac.jp>
2025-11-18 16:41:01 +0900

    練習８：追加した背景の反転

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_flip = pg.transform.flip(bg_img,True,False)
     kouka_img = pg.image.load("fig/3.png")
     kouka_img = pg.transform.flip(kouka_img,True,False)
     tmr = 0
@@ -17,12 +18,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        bg_lct = bg_img.get_rect()
-        bg_flip_lct = bg_img.get_rect()
-        bg_lct.center = 800-tmr,400
-        bg_flip_lct.center = 2400-tmr,400
-        screen.blit(bg_img, bg_lct)
-        screen.blit(bg_img,bg_flip_lct)
+        # bg_lct = bg_img.get_rect()
+        # bg_flip_lct = bg_img.get_rect()
+        # bg_lct.center = 800-tmr,400
+        # bg_flip_lct.center = 2400-tmr,400
+        # screen.blit(bg_img, bg_lct)
+        # screen.blit(bg_img,bg_flip_lct)
+        x = tmr
+        screen.blit(bg_img,[-x,0])
+        screen.blit(bg_img_flip,[1600-x,0])
         screen.blit(kouka_img,[300,200])
         pg.display.update()
         tmr += 1        
