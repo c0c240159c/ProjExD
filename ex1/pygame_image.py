48ec11bcb578e553c5e5f745eb389e283e25d47f
C0C24015 <c0c240159c@edu.teu.ac.jp>
2025-11-18 17:15:25 +0900

    演習課題１

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -31,6 +31,7 @@ def main():
         screen.blit(bg_img_flip,[1600-x,0])
         screen.blit(bg_img,[3200-x,0])
         screen.blit(kouka_img,kouka_lct)
+        kouka_lct.move_ip(-1,0)
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kouka_lct.move_ip(0,-1)
@@ -39,7 +40,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kouka_lct.move_ip(-1,0)
         if key_lst[pg.K_RIGHT]:
-            kouka_lct.move_ip(1,0)
+            kouka_lct.move_ip(2,0)
         pg.display.update()
         tmr += 1
         if tmr == 3199:
